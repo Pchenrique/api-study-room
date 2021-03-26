34147631f61d9a155915abb5022e2e77032962a7
Pchenrique <pchenrique1998@hotmail.com>
2021-03-26 15:40:27 -0300

    creation of the relationship model

---
+++ app/Models/ClassRoomUser.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model');
+
+class ClassRoomUser extends Model {}
+
+module.exports = ClassRoomUser;
