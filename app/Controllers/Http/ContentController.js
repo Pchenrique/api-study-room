264e83dc9013cd4d32daa7d4016ce50014229f1b
Nicolas Grisoste <ngrisoste@gmail.com>
2021-03-31 22:57:42 -0300

    Feat/listing comunications for class

---
+++ app/Controllers/Http/ContentController.js
@@ -0,0 +1,110 @@
+'use strict';
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Content = use('App/Models/Content');
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const ClassRoom = use('App/Models/ClassRoom');
+
+/**
+ * Resourceful controller for interacting with contents
+ */
+class ContentController {
+  /**
+   * Show a list of all contents.
+   * GET contents
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async listComunications({ params, response, auth }) {
+    const { classroomId } = params;
+    const { user } = auth;
+
+    try {
+      const room = await ClassRoom.findOrFail(classroomId);
+      const classroomsParticip = await user.classRooms().fetch();
+
+      const rooms = classroomsParticip.toJSON();
+      const roomCode = room.toJSON();
+
+      let confirmParticipation = false;
+
+      rooms.forEach((roomLine) => {
+        if (roomLine.id === roomCode.id) {
+          confirmParticipation = true;
+        }
+      });
+
+      if (!confirmParticipation) {
+        return response.status(409).json([
+          {
+            message: 'Student does not participate in this classroom',
+            field: 'classroom',
+            validation: 'participate',
+          },
+        ]);
+      }
+
+      const comunications = await Content.query()
+        .where('class_room_id', classroomId)
+        .with('commentsContents.user')
+        .fetch();
+
+      return response.status(200).json(comunications);
+    } catch (err) {
+      return response.status(404).json([
+        {
+          message: 'classroom not found',
+          fiels: 'classroom',
+          validation: 'not found',
+        },
+      ]);
+    }
+  }
+
+  /**
+   * Create/save a new content.
+   * POST contents
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) { }
+
+  /**
+   * Display a single content.
+   * GET contents/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async show({ params, request, response }) { }
+
+  /**
+   * Update content details.
+   * PUT or PATCH contents/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) { }
+
+  /**
+   * Delete a content with id.
+   * DELETE contents/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) { }
+}
+
+module.exports = ContentController;
